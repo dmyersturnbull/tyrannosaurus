b1c0f81f6b0400500b48bd7f5a5ddaeaf695c0d0
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2020-05-02 17:14:20 -0700

    Fixed readthedocs setup.

--- docs/conf.py
+++ docs/conf.py
@@ -5,31 +5,32 @@ Uses several extensions to get API docs and sourcecode.
 https://www.sphinx-doc.org/en/master/usage/configuration.html
 """
 
-# -- Docs setup --------------------------------------------------------------
-# Sphinx recommends adding modules for autodoc this way:
-# fmt: off
-import sys
 from pathlib import Path
 
+import tomlkit
+
+
 root = Path(__file__).parent.parent.absolute()
-sys.path.insert(0, str(root))
-from tyrannosaurus import __author__, __copyright__, __title__, __version__
-# fmt:on
+toml = tomlkit.loads((root / "pyproject.toml").read_text(encoding="utf8"))
+
+
+def find(key: str) -> str:
+    return str(toml["tool"]["poetry"][key])
+
+
+language = None
+project = find("name")
+version = find("version")
+release = find("version")
+author = ", ".join(find("authors"))
+copyright = "Copyright (2020)"
+
 
 extensions = ["autoapi.extension", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
 autoapi_type = "python"
-autoapi_dirs = [str(root / __title__)]
+autoapi_dirs = [str(root / project)]
 master_doc = "index"
 
-# -- Project information -----------------------------------------------------
 
-project = __title__
-copyright = __copyright__
-author = __author__
-version = __version__
-release = __version__
-language = None
-
-# -- General configuration ---------------------------------------------------
 exclude_patterns = ["_build", "Thumbs.db", ".*", "~*", "*~", "*#"]
 html_theme = "sphinx_rtd_theme"
